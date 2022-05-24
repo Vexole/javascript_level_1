fb83ecac85db9ab449db434a3d863418aed833fa
Bhupesh Shrestha <bhupesh.stha@gmail.com>
2022-05-24 17:36:30 -0400

    Implemented solution with basic js

---
+++ Assignment1/js/convert_temps.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let userInput;
+
+while (userInput != 999) {
+  userInput = parseFloat(
+    prompt(
+      "Enter Fahrenheit temperature \n Or enter 999 to terminate the conversion"
+    )
+  );
+
+  if (Number.isNaN(userInput)) {
+    alert(
+      "Please enter a valid number"
+    );
+  } else if (userInput != 999) {
+    let temperatureInCelsius = convertFahrenitToCelsius(userInput);
+    if (temperatureInCelsius) {
+      alert(
+        `Fahrenheit temperature is ${userInput} \n\n Celsius temperature is ${temperatureInCelsius}`
+      );
+    }
+  }
+}
+
+function convertFahrenitToCelsius(fahrenheit) {
+  if (fahrenheit < -100 || fahrenheit > 212) {
+    alert(`You entered ${fahrenheit}. \n Entry must range from -100 to +212.`);
+    return;
+  }
+
+  let temperatureInCelsius = (fahrenheit - 32) * (5 / 9);
+  return temperatureInCelsius.toFixed(2);
+}
